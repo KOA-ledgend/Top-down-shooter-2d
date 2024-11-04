f5e1598f6590f8cc51b1485689f184de19027029
KOA-ledgend <55025902+KOA-ledgend@users.noreply.github.com>
2024-11-03 23:28:08 -0500

    Makeing some changes to grid

--- Assets/Scrits/Game/Pathfinding/Grid.cs
+++ Assets/Scrits/Game/Pathfinding/Grid.cs
@@ -4,17 +4,39 @@ using UnityEngine;
 
 public class Grid : MonoBehaviour
 {
-    public Vector2 gridSize;
-    public float nodeRadius;
-    Nodes[,] grid;
+    public GameObject hexagonPrefab; // Your hexagon prefab
+    public int gridWidth = 10; // Number of hexagons horizontally
+    public int gridHeight = 10; // Number of hexagons vertically
+    public float hexagonSize = 1.0f; // Size of the hexagon
 
+    private float hexWidth;
+    private float hexHeight;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        hexWidth = hexagonSize * 2; // Width of the hexagon
+        hexHeight = Mathf.Sqrt(3) * hexagonSize; // Height of the hexagon
+
+        for (int q = 0; q < gridWidth; q++)
+        {
+            for (int r = 0; r < gridHeight; r++)
+            {
+                float xOffset = q * hexWidth * 0.75f; // Horizontal offset
+                float yOffset = r * hexHeight; // Vertical offset
+
+                // Adjust for odd/even rows to create the hex pattern
+                if (q % 2 == 1) yOffset += hexHeight / 2;
+
+                Vector2 position = new Vector2(xOffset, yOffset);
+                GameObject hex = Instantiate(hexagonPrefab, position, Quaternion.identity);
+                hex.name = $"Hex_{q}_{r}"; // Naming for reference
+            }
+        }
+
     }
 
+   
     // Update is called once per frame
     void Update()
     {
